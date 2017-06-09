6a66e604488f187dcb3ce7cf1949c995f3273674
Edward Kim <ekim@swin.edu.au>
2017-06-09 15:37:21 +1000

    Refactor BookStoreRepository using Repository traits

--- src/BookStoreRepository.php
+++ src/BookStoreRepository.php
@@ -4,102 +4,49 @@ namespace BookStore;
 
 use BookStore\Util\InternalPropertyAssignTrait;
 
+use BookStore\Repository;
 use PDO;
-use InvalidArgumentException;
 
 class BookStoreRepository
 {
-    use InternalPropertyAssignTrait;
+    use Repository\FindTrait;
+    use Repository\FindAllTrait;
+    use Repository\SaveUpdateTrait;
+    use Repository\RemoveTrait;
 
     protected $db;
+
+    protected $tableName = 'bookstore';
+    protected $key = 'id';
+    protected $entityType = BookStore::class;
+    protected $fields = [
+      'name',
+      'address',
+      'openedAt',
+    ];
 
     public function __construct(PDO $db)
     {
         $this->db = $db;
     }
 
-    public function find($id)
-    {
-        $stmt = $this->db->prepare('SELECT * FROM `bookstore` WHERE id = :id');
-        $stmt->bindParam(':id', $id);
-
-        $stmt->setFetchMode(PDO::FETCH_CLASS, BookStore::class); 
-        $stmt->execute();
-
-        $object = $stmt->fetch();
-        return $object !== false ? $object : null;
-    }
-
-    public function findAll()
-    {
-        $stmt = $this->db->prepare('SELECT * FROM `bookstore`');
-        $stmt->execute();
-
-        return $stmt->fetchAll(PDO::FETCH_CLASS, BookStore::class);
-    }
-    
     protected function convertToParams(BookStore $bookstore)
     {
-        $params = [
-            ':name' => $bookstore->getName(),
-            ':address' => $bookstore->getAddress(),
-            ':openedAt' => $bookstore->getOpenedAt(),
-        ];
-
-        if ($bookstore->getId() !== null) {
-          $params[':id'] = $bookstore->getId();
-        }
-
-        return $params;
+        return $this->convertToParamsByFields($bookstore, $this->fields, $this->key);
     }
 
     public function save(BookStore $bookstore)
     {
-        if ($bookstore->getId() !== null) {
-            return $this->update($bookstore);
-        }
-
-        $stmt = $this->db->prepare('INSERT INTO `bookstore`
-          SET
-            name = :name,
-            address = :address,
-            openedAt = :openedAt
-        ');
-        
-        $params = $this->convertToParams($bookstore);
-        $stmt->execute($params);
-
-        $id = $this->db->lastInsertId();
-        $this->assignInternalProperty($bookstore, 'id', $id);
+        return $this->saveEntity($bookstore);
     }
     
     public function update(BookStore $bookstore)
     {
-        $stmt = $this->db->prepare('UPDATE `bookstore`
-          SET
-            name = :name,
-            address = :address,
-            openedAt = :openedAt
-          WHERE
-            id = :id
-        ');
-
-        $params = $this->convertToParams($bookstore);
-        $stmt->execute($params);
+        return $this->updateEntity($bookstore);
     }
 
     public function remove(BookStore $bookstore)
     {
-        if ($bookstore->getId() === null) {
-            throw new InvalidArgumentException('BookStore id does not exist.');
-        }
-
-        $stmt = $this->db->prepare('DELETE FROM `bookstore` WHERE id = :id');
-
-        $stmt->execute([
-            ':id' => $bookstore->getId(),
-        ]);
-
-        return $stmt->rowCount() > 0;
+        return $this->removeEntity($bookstore);
     }
 }
