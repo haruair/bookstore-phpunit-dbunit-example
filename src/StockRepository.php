38cceaa111fd84e478277abefa18b649366067ac
Edward Kim <ekim@swin.edu.au>
2017-06-09 16:37:55 +1000

    Add invalid check test on stock repository

--- src/StockRepository.php
+++ src/StockRepository.php
@@ -3,6 +3,7 @@
 namespace BookStore;
 
 use PDO;
+use InvalidArgumentException;
 
 class StockRepository
 {
@@ -15,6 +16,10 @@ class StockRepository
 
     public function stockCount(Book $book, BookStore $bookStore = null)
     {
+        if ($book->getId() === null) {
+            throw new InvalidArgumentException("Invalid Book");
+        }
+
         $stmt = !is_null($bookStore) ? $this->getBookInStoreCountQuery() : $this->getBookCountQuery();
 
         $params = [
@@ -26,6 +31,30 @@ class StockRepository
 
         $stmt->execute($params);
         return $stmt->rowCount();
+    }
+
+    public function newStockTo(Book $book, BookStore $bookStore)
+    {
+        if ($book->getId() === null) {
+            throw new InvalidArgumentException("Invalid Book");
+        }
+
+        if ($bookStore->getId() === null) {
+            throw new InvalidArgumentException("Invalid BookStore");
+        }
+
+        $stmt = $this->db->prepare('INSERT INTO `stock`
+          SET
+            bookId = :bookId,
+            bookstoreId = :bookstoreId
+        ');
+
+        $params = [
+            ':bookId' => $book->getId(),
+            ':bookstoreId' => $bookStore->getId(),
+        ];
+
+        $stmt->execute($params);
     }
 
     protected function getBookCountQuery()
--- test/StockRepositoryTest.php
+++ test/StockRepositoryTest.php
@@ -7,11 +7,13 @@ use BookStore\Test\Util\DatabaseTestCase as TestCase;
 use BookStore\StockRepository;
 use BookStore\BookRepository;
 use BookStore\BookStoreRepository;
+use BookStore\Book;
+use BookStore\BookStore;
 
 class StockRepositoryTest extends TestCase
 {
     protected $fixtureFile = '/fixture/stock.xml';
-    
+
     private $book1;
     private $book2;
     private $bookstore1;
@@ -19,6 +21,8 @@ class StockRepositoryTest extends TestCase
 
     public function setUp()
     {
+        parent::setUp();
+
         $context = $this->getPDO();
         
         $bookRepository = new BookRepository($context);
@@ -49,5 +53,44 @@ class StockRepositoryTest extends TestCase
         $this->assertEquals($count1, 2);
         $this->assertEquals($count2, 3);
         $this->assertEquals($count3, 3);
+    }
+
+    public function testCreateNewStock()
+    {
+        $context = $this->getPDO();
+        $repository = new StockRepository($context);
+
+        $countBefore = $repository->stockCount($this->book1, $this->bookstore1);
+        $repository->newStockTo($this->book1, $this->bookstore1);
+        $countAfter = $repository->stockCount($this->book1, $this->bookstore1);
+
+        $this->assertEquals($countBefore, 2);
+        $this->assertEquals($countAfter, 3);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Invalid Book
+     */
+    public function testSaveAnInvalidBook()
+    {
+        $context = $this->getPDO();
+        $repository = new StockRepository($context);
+        $invalidBook = new Book;
+
+        $repository->newStockTo($invalidBook, $this->bookstore1);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     * @expectedExceptionMessage Invalid BookStore
+     */
+    public function testSaveAnInvalidBookStore()
+    {
+        $context = $this->getPDO();
+        $repository = new StockRepository($context);
+        $invalidBookstore = new BookStore;
+
+        $repository->newStockTo($this->book1, $invalidBookstore);
     }
 }
