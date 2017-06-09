c2b54ada804d90b7a29b19d35ad51d3a7bd72db4
Edward Kim <ekim@swin.edu.au>
2017-06-09 16:13:28 +1000

    Add stockcount method on StockRepository

--- src/StockRepository.php
+++ src/StockRepository.php
@@ -2,7 +2,39 @@
 
 namespace BookStore;
 
+use PDO;
+
 class StockRepository
 {
+    protected $db;
 
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    public function stockCount(Book $book, BookStore $bookStore = null)
+    {
+        $stmt = !is_null($bookStore) ? $this->getBookInStoreCountQuery() : $this->getBookCountQuery();
+
+        $params = [
+            ':bookId' => $book->getId(),
+        ];
+        if ($bookStore !== null) {
+            $params[':bookstoreId'] = $bookStore->getId();
+        }
+
+        $stmt->execute($params);
+        return $stmt->rowCount();
+    }
+
+    protected function getBookCountQuery()
+    {
+        return $this->db->prepare('SELECT * FROM `stock` WHERE bookId = :bookId');
+    }
+
+    protected function getBookInStoreCountQuery()
+    {
+        return $this->db->prepare('SELECT * FROM `stock` WHERE bookId = :bookId AND bookstoreId = :bookstoreId');
+    }
 }
--- test/StockRepositoryTest.php
+++ test/StockRepositoryTest.php
@@ -5,15 +5,49 @@ namespace BookStore\Test;
 use BookStore\Test\Util\DatabaseTestCase as TestCase;
 
 use BookStore\StockRepository;
+use BookStore\BookRepository;
+use BookStore\BookStoreRepository;
 
 class StockRepositoryTest extends TestCase
 {
     protected $fixtureFile = '/fixture/stock.xml';
+    
+    private $book1;
+    private $book2;
+    private $bookstore1;
+    private $bookstore2;
+
+    public function setUp()
+    {
+        $context = $this->getPDO();
+        
+        $bookRepository = new BookRepository($context);
+        $bookstoreRepository = new BookStoreRepository($context);
+
+        $this->book1 = $bookRepository->find(1);
+        $this->book2 = $bookRepository->find(2);
+        $this->bookstore1 = $bookstoreRepository->find(1);
+        $this->bookstore2 = $bookstoreRepository->find(2);
+    }
 
     public function testCanCreate()
     {
         $context = $this->getPDO();
         $repository = new StockRepository($context);
         $this->assertInstanceOf(StockRepository::class, $repository);
+    }
+
+    public function testFindStockCount()
+    {
+        $context = $this->getPDO();
+        $repository = new StockRepository($context);
+
+        $count1 = $repository->stockCount($this->book1, $this->bookstore1);
+        $count2 = $repository->stockCount($this->book2, $this->bookstore1);
+        $count3 = $repository->stockCount($this->book1);
+        
+        $this->assertEquals($count1, 2);
+        $this->assertEquals($count2, 3);
+        $this->assertEquals($count3, 3);
     }
 }
