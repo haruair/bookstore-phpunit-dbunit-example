1e6186a352e9b5fb6016824b3eab4e0a7496ad46
Edward Kim <ekim@swin.edu.au>
2017-06-09 14:30:18 +1000

    Add FindById on BookStoreRepository

--- src/BookStoreRepository.php
+++ src/BookStoreRepository.php
@@ -12,4 +12,16 @@ class BookStoreRepository
     {
         $this->db = $db;
     }
+
+    public function find($id)
+    {
+        $stmt = $this->db->prepare('SELECT * FROM `bookstore` WHERE id = :id');
+        $stmt->bindParam(':id', $id);
+
+        $stmt->setFetchMode(PDO::FETCH_CLASS, BookStore::class); 
+        $stmt->execute();
+
+        $object = $stmt->fetch();
+        return $object !== false ? $object : null;
+    }
 }
--- test/BookStoreRepositoryTest.php
+++ test/BookStoreRepositoryTest.php
@@ -3,7 +3,9 @@
 namespace BookStore\Test;
 
 use BookStore\Test\Util\DatabaseTestCase as TestCase;
+
 use BookStore\BookStoreRepository;
+use BookStore\BookStore;
 
 class BookStoreRepositoryTest extends TestCase
 {
@@ -14,5 +16,20 @@ class BookStoreRepositoryTest extends TestCase
         $context = $this->getPDO();
         $repository = new BookStoreRepository($context);
         $this->assertInstanceOf(BookStoreRepository::class, $repository);
+    }
+
+    public function testCanFindBookStoreById()
+    {
+        $context = $this->getPDO();
+        $repository = new BookStoreRepository($context);
+
+        $bookStoreFirst = $repository->find(1);
+        $bookStoreSecond = $repository->find(2);
+
+        $this->assertInstanceOf(BookStore::class, $bookStoreFirst);
+        $this->assertEquals($bookStoreFirst->getName(), 'Polarbear Bookstore');
+
+        $this->assertInstanceOf(BookStore::class, $bookStoreSecond);
+        $this->assertEquals($bookStoreSecond->getName(), 'PHP Bookstore');
     }
 }
